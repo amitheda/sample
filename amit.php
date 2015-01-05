693637e76dcdde7774e9ad5d2317f51391734cae
root <root@local-deb.local-deb>
2015-01-04 22:09:14 -0800

    added html tags

--- amit.php
+++ amit.php
@@ -2,18 +2,5 @@
 
 <?php
  echo"Hello amit heda";
- echo "<!DOCTYPE html>
- <html>
- <head>
- 	<meta charset="utf-8">
-  	<title>My Git web page</title>
- 
- </head>
- <body>
- 	<h1>hi buddy</h1>
- </body>
- </html>";
-
-
-?>
+ ?>
  
